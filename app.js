6beea71de3d9349bb7211323c9f5bca9b2d614ad
Arati Sureddi <aps913@Aratis-MacBook-Pro-2.local>
2015-04-05 17:06:33 -0700

    andy spec for method post

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require("pg");
+var methodOverride = require("method-override");
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // Refactor connection and query code
 var db = require("./models");
 
-app.get('/articles', function(req,res) {
-  console.log("GET /articles");
-});
+//needs to display summary of all articles
+app.get('/articles', function(req, res) {
+    db.Article.findAll().then(function(articles) {
+        res.render('articles/index', { articlesList: articles });
+   })  // added paren here
+}); 
 
+//displays form to submit articles
 app.get('/articles/new', function(req,res) {
   res.render('articles/new');
 });
 
+//user should not see this page, just used for internal routing
 app.post('/articles', function(req,res) {
-  console.log(req.body);
+  db.Article.create({title req.body.article.title, author: req.body.article.author}).then(function (article) {
+	res.redirect("/articles");
+	});
+  //console.log(req.body);
 });
 
+
+
+
+//to find an article by ID
 app.get('/articles/:id', function(req, res) {
-  console.log(req.body);
+  res.render(req.body);
   
 })
 
+//displays homepage at localhost3000
 app.get('/', function(req,res) {
   res.render('site/index.ejs');
 });
 
+//displays about page
 app.get('/about', function(req,res) {
   res.render('site/about');
 });
 
+//displays contact page
 app.get('/contact', function(req,res) {
   res.render('site/contact');
 });
 
+//tells the browser where to receive info
 app.listen(3000, function() {
   console.log('Listening');
 });
